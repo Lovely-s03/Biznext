0baf1bf21113f3f790ead03c3768706de24f0f2f
Lovely-s03 <supriyalovely0306@gmail.com>
2025-10-06 15:17:35 +0530

    dashboard

--- src/Component/Pages/UserDashboard.jsx
+++ src/Component/Pages/UserDashboard.jsx
@@ -1,13 +1,15 @@
+
+
 import React from "react";
 import { Outlet } from "react-router-dom";
-import Sidebar from "../SideBar";
-import Navbar from "../Navbar";
+
+import Sidebar from '../Sidebar'
 
 const UserDashboard = () => {
   return (
     <div className="flex h-screen">
       
-      <Sidebar />
+      <Sidebar/>
 
       <main className="flex-1 p-6 bg-gray-100 overflow-y-auto">
         <Outlet /> 
