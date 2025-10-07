464ea35898bce08f8c0d907ab8d8e8ed27b565be
Lovely-s03 <supriyalovely0306@gmail.com>
2025-10-07 16:07:38 +0530

    this

--- src/Component/Routes/UserRoutes.jsx
+++ src/Component/Routes/UserRoutes.jsx
@@ -1,14 +1,11 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 
-import Home from "../Pages/User/Home";
-import Profile from "../Pages/User/Profile";
-import Settings from "../Pages/User/Setting";
+
 import PrivateRoute from "../PrivateRoute";
 import UserDashboard from "../Pages/UserDashboard";
 import Dashboard from "../Pages/User/Dashboard/Dashboard";
 import AyushPay from "../Pages/User/AyushPay";
-import DTHConnection from "../Pages/User/DTHConnection/DTHConnection";
 import Dth from "../Pages/User/DTHConnection/Dth";
 
 const UserRoutes = () => {
@@ -28,9 +25,7 @@ const UserRoutes = () => {
 
 
 <Route path="/dth" element={<Dth/>} />
-        <Route path="home" element={<Home />} />
-        <Route path="profile" element={<Profile />} />
-        <Route path="settings" element={<Settings />} />
+       
       </Route>
     </Routes>
   );
